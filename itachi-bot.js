d8973a2bc36b78d25aa3e7e3a730ab91eb99e368
Jootsu <luthermikasa@gmail.com>
2024-12-02 19:08:30 +0200

    Update itachi-bot.js

--- itachi-bot.js
+++ itachi-bot.js
@@ -1,16 +1,17 @@
-const bot = require(__dirname + '/lib/smd')
-const { VERSION } = require(__dirname + '/config')
+    javascript
+    const bot = require(__dirname + '/lib/smd');
+    const { VERSION } = require(__dirname + '/config');
 
-const start = async () => {
-    Debug.info(`ITACHI ${VERSION}`)
-  try {
-    await bot.init()
-    bot.logger.info('⏳ Database syncing!')
-    await bot.DATABASE.sync()
-    await bot.connect()
-  } catch (error) {
-    Debug.error(error);
+    const start = async () => {
+        console.info(`ITACHI ${VERSION}`);
+        try {
+            await bot.init();
+            bot.logger.info('⏳ Database syncing!');
+            await bot.DATABASE.sync();
+            await bot.connect();
+        } catch (error) {
+            console.error(error);
+            start();
+        }
+    }
     start();
-  }
-}
-start();
